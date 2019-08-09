cdcc2727abaf1a95cba58fb34fba3a0815b32423
ayshamarty <ayshamarty@gmail.com>
2019-08-09 11:45:31 +0100

    begun setup

--- src/main/java/com/bae/pokeapi/service/UserService.java
+++ src/main/java/com/bae/pokeapi/service/UserService.java
@@ -1,5 +1,11 @@
 package com.bae.pokeapi.service;
 
+import org.springframework.stereotype.Service;
+
+import com.bae.pokeapi.persistence.domain.User;
+
+@Service
 public interface UserService {
+	public User createUser(User user);
 
 }
