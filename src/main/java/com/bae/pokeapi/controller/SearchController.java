b91c7bf98c40e85b1d5446b7de6ef0ada443a45d
ayshamarty <ayshamarty@gmail.com>
2019-08-09 12:50:52 +0100

    connected source

--- src/main/java/com/bae/pokeapi/controller/SearchController.java
+++ src/main/java/com/bae/pokeapi/controller/SearchController.java
@@ -1,5 +1,32 @@
 package com.bae.pokeapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.pokeapi.service.SearchService;
+
+@RestController
+@RequestMapping("/Search")
 public class SearchController {
+
+	public SearchController() {
+	}
+
+	public SearchService service;
+
+	@Autowired
+	public SearchController(SearchService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/pokemon/{pokemon}")
+	public ResponseEntity<Object> findPokemon(@PathVariable String pokemon) {
+		return new ResponseEntity<>(service.findPokemon(pokemon), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/bae/pokeapi/service/SearchServiceImplementation.java
+++ src/main/java/com/bae/pokeapi/service/SearchServiceImplementation.java
@@ -1,5 +1,40 @@
 package com.bae.pokeapi.service;
 
-public class SearchServiceImplementation {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SearchServiceImplementation implements SearchService {
+
+	private RestTemplate restTemplate;
+
+	@Autowired
+	public SearchServiceImplementation(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@Override
+	public Object findPokemon(String pokemon) {
+		ResponseEntity<Object> pokemonFound = restTemplate
+				.exchange("http://localhost:8082/Source/getPokemon/" + pokemon, HttpMethod.GET, null, Object.class);
+		return pokemonFound.getBody();
+	}
+
+	@Override
+	public Object findType(String type) {
+		ResponseEntity<Object> typeFound = restTemplate.exchange("http://localhost:8082/Source/getPokemon/" + type,
+				HttpMethod.GET, null, Object.class);
+		return typeFound.getBody();
+	}
+
+	@Override
+	public Object findAbility(String ability) {
+		ResponseEntity<Object> abilityFound = restTemplate
+				.exchange("http://localhost:8082/Source/getPokemon/" + ability, HttpMethod.GET, null, Object.class);
+		return abilityFound.getBody();
+	}
 
 }
