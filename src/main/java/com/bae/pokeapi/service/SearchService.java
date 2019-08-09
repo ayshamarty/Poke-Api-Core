fcbeed82bf93edca05eb8649a283c2d1a062a417
ayshamarty <ayshamarty@gmail.com>
2019-08-09 14:42:36 +0100

    producer working

--- src/main/java/com/bae/pokeapi/service/SearchService.java
+++ src/main/java/com/bae/pokeapi/service/SearchService.java
@@ -1,5 +1,7 @@
 package com.bae.pokeapi.service;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,7 @@ public interface SearchService {
 	public Object findType(String type);
 
 	public Object findAbility(String ability);
+
+	public void sendToQueue(Long id, String searchTerm, Date timeStamp);
 
 }
